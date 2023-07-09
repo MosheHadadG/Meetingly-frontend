e36cd3e61533dcddabd65ca38b95c5457bf39ef7
Moshe <102148360+MosheHadadG@users.noreply.github.com>
2023-07-09 23:56:22 +0300

    Change url api to ec2

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,6 @@
 const prod = {
   url: {
-    API_URL: "https://meetingly-backend.onrender.com",
+    API_URL: "https://meetingly.api.my.to",
     SOCKET_URL: "https://meetingly-socket.onrender.com",
   },
 };
